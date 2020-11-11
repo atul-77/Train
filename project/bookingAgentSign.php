5c1f03727d3187f14e05b0e2c55bca3eeeb18c48
root <root@pop-os.localdomain>
2020-11-11 15:27:43 +0530

    pushing again

--- project/bookingAgentSign.php
+++ project/bookingAgentSign.php
@@ -11,7 +11,7 @@
     <div class="container">
         <h1>Welcome to Signup page for Booking portal</h1>
         <p>Please Enter your details to proceed</p>
-        <form action="mainPage.php" method="post">
+        <form action="mainpage.php" method="post">
             <input type="text" name="name" id="naam" placeholder="Enter your name">
             <input type="text" name="address" id="jagah" placeholder="Enter your address">
             <input type="text" name="credit" id="credit" placeholder="Enter your credit card number">
