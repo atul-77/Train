ed2c2644efc258a34f51bc1c352e1f75f8c91215
root <root@pop-os.localdomain>
2020-11-11 17:41:41 +0530

    redirection added

--- project/php/mainpage.php
+++ project/php/mainpage.php
@@ -4,13 +4,16 @@
     $name  = $_POST['name'];
     $credit  = $_POST['credit'];
     $address = $_POST['address'];
+    // $datacheck = "select CreditCardNo. from BookingAgent";
+    // echo $con->query($datacheck); 
     $sql = "INSERT INTO `Train`.`BookingAgent` (`CreditCardNo.`, `Name`, `Adress`) VALUES ('$credit', '$name', '$address');";
     if($con->query($sql)==true){
         echo "Successfully updated database";
     }
     else{
-        echo "Error bish: $con->error";
+        // echo "You are already signed up dumbass";
+        header("Location: /project/bookingAgentLogin.php");
+        exit();
     }
-
     $con->close();
 ?>
