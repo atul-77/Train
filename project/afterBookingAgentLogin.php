b2e06e069884837b95321baf982916933a60ae12
Adarsh Kumar <46217049+adarshcsb66@users.noreply.github.com>
2020-11-20 23:57:46 +0530

    Different types of train searching done

--- project/afterBookingAgentLogin.php
+++ project/afterBookingAgentLogin.php
@@ -10,13 +10,17 @@
 <body>
     <div class="container">
         <h1>Railway Reservation System</h1>
-        <h3>Trains Between Stations</h3>
+        <h2>Trains Between Stations</h2>
         <form action="php\afterBookingAgent.php" method="post">
+            <p>You can either enter <b>only the Date</b> and click submit to see all trains running on that date<br>
+                or you can enter the <b>Start and End stations only</b> and see all trains running between those stations regardless of the date<br>
+                or you can enter <b>all the details</b> i.e Date, Start, and End Stations.
+            </p>
             <p><b>Enter the date for which you want to view trains:</b></p>
             <input type="date" name="date" id="date" placeholder="Enter the date for which you want to book the ticket">
-            <p><b>Enter the start station of your journey</b></p>
+            <p><b>Enter the start station of your journey:</b></p>
             <input type="startstn" name="startstn" id="startstn" placeholder="Enter departure station name">
-            <p><b>Enter the end station of your journey</b></p>
+            <p><b>Enter the end station of your journey:</b></p>
             <input type="endstn" name="endstn" id="endstn" placeholder="Enter arrival station name">
             <br>
             <button class="btn" name="Submit">Submit</button>
